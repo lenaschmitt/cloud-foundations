77ee2de64076e7c73e419068a21c194ca57e0d30
lenaschmitt <99411956+lenaschmitt@users.noreply.github.com>
2022-03-04 13:06:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,13 +14,13 @@ def name(value):
     return jsonify(val)
 
 @app.route('/html')
- def html():
- """Returns some custom HTML"""
- return """
- <title>This is a Hello World World Page</title>
- <p>Hello</p>
- <p><b>World</b></p>
- """
+def html():
+    """Returns some custom HTML"""
+    return """
+    <title>This is a Hello World World Page</title>
+    <p>Hello</p>
+    <p><b>World</b></p>
+    """
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
